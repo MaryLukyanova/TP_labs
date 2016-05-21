af301b7a75a462983e3cf95721935476953b7720
MaryLukyanova <bobbkrist@gmail.com>
2016-05-21 07:19:00 +0400

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tplab7_2
+//
+//  Created by Admin on 07.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  tplab7_2
+//
+//  Created by Admin on 07.05.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
